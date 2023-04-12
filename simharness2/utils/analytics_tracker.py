eaebc6aa30603edd0f7c5a50e8517f0f4207ab0f
Aidan Fennelly <afennelly@mitre.org>
2023-04-12 19:53:57 +0000

    WIP commit for AnalyticsTracker class and configs

--- simharness2/utils/analytics_tracker.py
+++ simharness2/utils/analytics_tracker.py
@@ -1,194 +1,157 @@
-# FiReLine Environment Analysis & Reward (FEAR) Data Class to dynamically extract, store and transform key variable data from RL Agent Training Experiments
-#                                                                to create Config reward capabilites and offer additional experimental analysis
-#                                                                                                                                   - (dgandikota)
+"""FIXME: A one line summary of the module or program.
 
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
+
+Typical usage example:
+
+  foo = ClassFoo()
+  bar = foo.FunctionBar()
+"""
 from abc import ABC, abstractmethod
-# other possible names - FEAR_D : FiReLine Environment Analysis & Reward Data
-#                     - FETA : FiReLine Environment Training Analysis
-from collections import OrderedDict as ordered_dict
-from copy import deepcopy
-from typing import Dict, List, OrderedDict, Tuple
+from typing import Any, Dict, List
 
 import numpy as np
 from simfire.enums import BurnStatus
 from simfire.sim.simulation import Simulation
 
-# define and update this class object within the environment file code
+from .tracker_config import SimpleSimulationMetricsTracker
+
+# NOTE on Terminology:
+#  - "Run" == a single experiment with a single set of parameters
+#  - "Episode" == a single episode within a run
+#  - "Timestep" == a single timestep within an episode
 
 
-class AnalyticsTracker:
-    def __init__(self, agent_speed, sim_size):
+# class BaseTracker(ABC):
+# Other names: `AnalyticsData`, RLHarnessDataStore`, `RLHarnessData`?
+class AnalyticsTracker(ABC):
+    """TODO Add class docstring."""
 
-        # VARIABLES TRACKED ACROSS THE ENTIRE EXPERIMENT - ALL THE SIMULATIONS
-        # -------------------------------------------------------------------
-
-        ##  lowest number of undamaged squares achieved across all of the simulations in an experiment
-        self.lowest_exp_undamaged = 0
-
-        ## lowest number of timesteps used to stop fire
-        self.lowest_timesteps
-
-        # VARIABLES TRACKED ACROSS THE FULL RUN OF A SIMULATION
-        # -------------------------------------------------------------------
-
-        ##  list that stores the number of damaged squares during each timestep of the same simulation with no agent mitigation
-        self.damaged_per_timestep_benchmarkSim = [0]
-
-        ##  the speed of the agent in the current simulation
-        self.agent_speed = agent_speed
-
-        ## the simulation size in area (num squares)
-        self.sim_area = sim_size
-
-        # VARIABLES TRACKED/UPDATED FOR EACH SIMULATION TIMESTEP WITH NO AGENT - BENCHMARK SIMULATION
-        # -------------------------------------------------------------------
-
-        ##  number of timesteps that have occurred in the current benchmark simulation
-        self.num_timesteps_benchmarkSim = 1
-
-        ##  number of new squares that are burning in current timestep of benchmark simulation
-        self.recent_damaged_benchmarkSim = 1
-
-        ## track whether bench_sim is active
-        self.bench_sim_active = True
-
-        # VARIABLES TRACKED/UPDATED FOR EACH SIMULATION TIMESTEP WITH AGENT - AGENT SIMULATION
-        # -------------------------------------------------------------------
-
-        ##  number of timesteps that have occurred in the current simulation
-        self.num_timesteps = 1
-
-        ##  number of steps the agent has taken in the current simulation
-        self.num_agent_steps = 0
-
-        ## track whether simulation with agent is active
-        self.sim_active = True
-
-        ##  the total number of squares that have been burned in the simulation
-        self.num_burned = 0
-
-        ##  the number of squares that are currently burning in the simulation
-        self.burning = 0
-
-        ##  the number of squares that are burned in the most recent timestep of the simulation
-        self.recent_burned = 0
-
-        ##  the number of squares that are burning in the most recent timestep of the simulation
-        self.recent_burning = 0
-
-        ##  the total number of squares that have mitigations placed by the agent(s)
-        self.num_mitigations = 0
-
-        ##  the number of mitigations placed in the most recent timestep
-        self.recent_mitigations
-
-        ##  the number of remaining squares in the simulation in this timestep that are undamaged
-        ###   the number of squares that neither have a mitigation or were burned/burning
-        self.num_undamaged
-
-        ##  number of new squares that are damaged in current timestep of Agent simulation
-        self.recent_damaged = 1
-
-        ##  did the agent place a mitigation in this timestep
-        self.mitigation_placed = False
-
-        ##  is the agent in a burning square within this timestep
-        self.agent_burning = False
-
-        ##  is the agent in a square that has already been burned within this timestep
-        self.agent_in_burned_area = False
-
-        ##  is the agent close to a burning square within this timestep
-        self.agent_near_burning_area = False
-
-    # Update the FEAR Data Class Variables after each timestep of the Benchmark Simulation
-    def timestep_BenchSim_FEAR_Update(
-        self, timestep, Benchmark_fire_map, Benchmark_sim_active
+    def __init__(
+        self,
+        # TODO add type anns to input arguments
+        sim_data,
+        sim_screen_size: int,
+        run_data=None,
+        agent_data=None,
+        bench_sim_data=None,
     ):
+        """TODO (afennelly): Add docstring.
 
-        # TODO integrate functionality for when the bench sim is not active
+        Expected usage:
+            TODO
 
-        self.bench_sim_active = Benchmark_sim_active
+        Arguments:
+            sim_data: TODO
+            sim_screen_size: An int representing how large the simulation is in pixels.
+              The screen_size sets both the height and the width of the screen.
+            run_data: TODO
+            agent_data: TODO
+            bench_sim_data: TODO
+        """
+        # Required attributes that track simulation data across each episode in a run.
+        self.sim_data = sim_data
+        self.sim_screen_size = sim_screen_size
+        # Optional attributes that track additional data for the RLHarness.
+        self.run_data = run_data
+        self.agent_data = agent_data
+        self.bench_sim_data = bench_sim_data
 
-        self.num_timesteps_benchmarkSim = timestep
-
-        # Use the fire_map to calculate the number of currently burning squares in the benchmark sim
-        num_burning_Benchmark = np.count_nonzero(Benchmark_fire_map == BurnStatus.BURNING)
-
-        # add in the value of the # of NEW burning squares into the self.damaged_per_timestep_benchmarkSim list for this timestep
-        self.damaged_per_timestep_benchmarkSim[self.num_timesteps_benchmarkSim] = (
-            num_burning_Benchmark
-            - self.damaged_per_timestep_benchmarkSim[self.num_timesteps_benchmarkSim - 1]
-        )
-
-        # define the self.recent_damaged_benchmarkSim as this value
-        self.recent_damaged_benchmarkSim = self.damaged_per_timestep_benchmarkSim[
-            self.num_timesteps_benchmarkSim
-        ]
-
-    # Update the FEAR Data Class Variables after each Agent action/movement of the Agent Simulation - Useful when Agent Speed is greater than 1
-    def AgentStep_FEAR_Update(
-        self, timestep, simulation, mitigation_placed=False, nearby_fire=False
+    @abstractmethod
+    def update_after_one_simulation_step(
+        self,
+        sim_map: np.ndarray,
+        sim_active: bool,
+        bench_sim_map: np.ndarray,
+        bench_sim_active: bool,
+        **kwargs,
     ):
+        """TODO Add docstring."""
+        raise NotImplementedError
 
-        self.num_timesteps = timestep
-
-        self.num_agent_steps = self.num_agent_steps + 1
-
-        self.mitigation_placed = mitigation_placed
-
-        if self.mitigation_placed:
-            self.recent_mitigations = self.recent_mitigations + 1
-
-        # make sure that this fire_map is has the agent's most recent mitigation recorded
-
-        # TODO
-        self.agent_burning = False
-        # track this to add big negative reward
-
-        # TODO
-        self.agent_in_burned_area = False
-        # subtract from recent_mitigations if the mitigation was placed in a burned area
-
-        # TODO
-        self.agent_near_burning_area = nearby_fire
-        # track this to add negative reward for Agent being too close to fire
-
-    # Update the FEAR Data Class Variables after each timestep of the Agent-Simulation
-    def timestep_AgentSim_FEAR_Update(
-        self, timestep, AgentSim_fire_map, AgentSim_sim_active
+    @abstractmethod
+    def update_after_one_agent_step(
+        self,
+        agent_pos: List[int],
+        sim_map: np.ndarray,
+        **kwargs,
     ):
+        """TODO Add docstring."""
+        raise NotImplementedError
 
-        self.num_timesteps = timestep
 
-        self.sim_active = AgentSim_sim_active
+"""The status of each pixel in a `fire_map`
 
-        # make sure that this fire_map is has all the agent's most recent mitigation recorded and has run through the simulation to see the effects
-        # get the burned and burning squares from this fire_map
-        burned = np.count_nonzero(AgentSim_fire_map == BurnStatus.BURNED)
+Current statuses are:
+    - UNBURNED
+    - BURNING
+    - BURNED
+    - FIRELINE
+    - SCRATCHLINE
+    - WETLINE
 
-        burning = np.count_nonzero(AgentSim_fire_map == BurnStatus.BURNING)
+UNBURNED: int = 0
+BURNING: int = auto()
+BURNED: int = auto()
+FIRELINE: int = auto()
+SCRATCHLINE: int = auto()
+WETLINE: int = auto()
+"""
 
-        # Use the stored previous values of burning and num_burned to calculate the NEW burning and burned squares in this timestep
-        self.recent_burning = burning - self.burning
 
-        self.recent_burned = burned - self.num_burned
+class SimpleAnalyticsTracker(AnalyticsTracker):
+    """TODO Add class docstring."""
 
-        # update the stored values of burned and num_burned from this new fire_map
-        self.num_burned = np.count_nonzero(AgentSim_fire_map == BurnStatus.BURNED)
+    def __init__(self, sim_data, sim_screen_size: int):
+        """TODO (afennelly): Add docstring.
 
-        self.burning = np.count_nonzero(AgentSim_fire_map == BurnStatus.BURNING)
+        Expected usage:
+            TODO
 
-        self.num_mitigations = self.num_mitigations + self.recent_mitigations
+        Arguments:
+            sim_data: TODO
+            sim_screen_size: An int representing how large the simulation is in pixels.
+              The screen_size sets both the height and the width of the screen.
+        """
+        super().__init__(sim_data, sim_screen_size)
 
-        # Calculate number of squares damaged in this timestep and the total undamaged squares left
-        self.recent_damaged = (
-            self.recent_burning + self.recent_burned + self.recent_mitigations
-        )
+    def update_after_one_simulation_step(
+        self, timestep: int, sim_map: np.ndarray, **kwargs
+    ):
+        # Assuming we use `SimpleReward`, what variables do we need to calculate the reward?
+        # - num_burning_per_step
+        self.sim_data.update(timestep, sim_map, self.sim_screen_size)
 
-        self.num_undamaged = (
-            self.sim_area - self.burning - self.burned - self.num_mitigations
-        )
 
-    # TODO
-    # create method to inheret the "VARIABLES TRACKED ACROSS THE ENTIRE EXPERIMENT" when a new reward_class object is defined for each new simulation within an experiment
+# class SimpleAnalyticsTrackerWithAgentData(SimpleAnalyticsTracker):
+#     # previously named `AgentStep_FEAR_Update()`
+#     # better name I think: `update_variables_tracked_after_one_agent_step()`
+#     def update_after_one_agent_step(
+#         self,
+#         agent_pos: List[int],
+#         sim_map: np.ndarray,
+#         interaction: bool,
+# #     ):
+#         """Run after one "agent step" is taken within the simulation.
+
+#         We consider one "agent step" to be when the agent performs one movement,
+#         followed by one interaction with the environment. Recall that "don't move" and
+#         "don't interact" are valid agent movements and interactions, respectively, so
+#         the agent's position may not change after one "agent step".
+
+#         NOTE: Currently, one "agent step" is taken for every call to the environment's
+#         `step()` method.
+
+#         Arguments: FIXME update docstring with new arguments
+#             timestep: An integer representing the current timestep of the simulation.
+#             mitigation_placed: A boolean indicating whether agent's interaction with the
+#               environment resulted in a mitigation being placed.
+#             nearby_fire: A boolean indicating whether the agent is in a square that is
+#                 currently burning or has been burned in the past. This is used to determine
+#                 whether the agent is in a "danger zone" and should be penalized for not.
+
+#         """
+#         self.agent_data.update(agent_pos, sim_map, interaction)
---
+++ simharness2/utils/tracker_config.py
@@ -0,0 +1,79 @@
+from typing import List
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from numpy import ndarray
+
+
+class SimulationMetricsTracker(ABC):
+    """TODO Add class docstring."""
+
+    @abstractmethod
+    def update(self, sim_map: ndarray):
+        """TODO Add docstring."""
+        raise NotImplementedError
+
+
+# @dataclass
+class SimpleSimulationMetricsTracker(SimulationMetricsTracker):
+    # Indicates whether the simulation has reached the end.
+    active: bool = True
+    # Number of times the simulation has been run (ie. `FireSimulation.run()`).
+    # num_steps: int = 0
+    # Number of spaces in `sim.fire_map` that have `BurnStatus.BURNING`.
+    num_burning: int = 0
+    # Array
+    # num_burning_per_step: ndarray
+
+    def update(self, sim_map: ndarray):
+        
+
+
+@dataclass
+class SimulationMetricsTracker:
+    active: bool = True
+    num_steps: int = 0
+    # Number of spaces in sim_map that are not equal to BurnStatus.UNBURNED
+    num_damaged_per_step: ndarray
+    # num new squares damaged (since last timestep)
+    # num_damaged_diff: int
+    # num "burned" squares
+    num_burned: int
+    num_new_burned: int
+    # - num new "burned" squares (since last timestep)
+    num_currently_burning: int
+    # - num "currently burning" squares
+    num_new_currently_burning: int
+    # - num new "currently burning" squares (since last timestep)
+    num_mitigation_lines: int
+    # - num mitigation lines
+    num_new_mitigation_lines: int
+    # - num new mitigation lines (since last timestep)
+
+
+@dataclass
+class AgentMetricsTracker:
+    num_steps: int = 0
+    # TODO decide how to initialize array
+    # - Maybe an empty array with size == to max steps for a given episode?
+    # - Otherwise, we will have to append to the array on each timestep. Costly??
+    is_burning_per_step: ndarray
+    in_burned_area_per_step: ndarray
+    near_burned_area_per_step: ndarray
+
+    def update(self, agent_pos: List[int], sim_map: ndarray, interaction: bool):
+        self.num_steps += 1
+
+        # TODO put num_mitigations and recent_mitigations in the SimMetricsTracker?
+        if interaction:
+            self.recent_mitigations += 1
+
+        # TODO track this to add big negative reward
+        self.agent_burning = False
+        # TODO subtract from self.recent_mitigations if the mitigation was placed in a burned area
+        self.agent_in_burned_area = False
+        # TODO track this to add negative reward for Agent being too close to fire
+        self.agent_near_burning_area = _nearby_fire(sim_map, agent_pos)
+
+    def _nearby_fire(sim_map: ndarray, agent_pos: List[int]) -> bool:
+        # TODO
+        return False
