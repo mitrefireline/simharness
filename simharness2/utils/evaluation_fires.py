b53dd3d53c0f56b615701f3e8289562d23bdac67
Aidan Fennelly <afennelly@mitre.org>
2023-05-23 01:55:21 +0000

    somehow, the rest of the file wasn't committed

--- simharness2/utils/evaluation_fires.py
+++ simharness2/utils/evaluation_fires.py
@@ -102,4 +102,55 @@ def get_default_operational_fires(
         for _ in range(num_fire_positions)
     ]
     # Generate valid positions for agent start
-   
+    agent_positions = [
+        tuple(rng.integers(0, screen_size, size=2, dtype=int))
+        for _ in range(num_agent_positions)
+    ]
+
+    # Create operational fire scenarios using the generated locations and positions
+    op_fires = {}
+    num_op_fires = 0
+    for op_loc in op_locations:
+        fire_positions = rng.permutation(fire_positions)
+        agent_positions = rng.permutation(agent_positions)
+        for idx in range(len(fire_positions)):
+            op_fires[num_op_fires] = (
+                op_loc,
+                tuple(fire_positions[idx]),
+                tuple(agent_positions[idx]),
+            )
+            num_op_fires += 1
+
+    return op_fires
+
+
+def np_random(seed: int) -> Tuple[np.random.Generator, Any]:
+    """Generates a random number generator from the seed and returns the Generator and seed.
+
+    Args:
+        seed: The seed used to create the generator
+
+    Returns:
+        The generator and resulting seed
+
+    Raises:
+        Error: Seed must be a non-negative integer or omitted
+    """
+    seed_seq = np.random.SeedSequence(seed)
+    np_seed = seed_seq.entropy
+    rng = np.random.Generator(np.random.PCG64(seed_seq))
+    return rng, np_seed
+
+
+def _try_to_create_sim(
+    sim_yaml_data: DictConfig, seed: int, rng: np.random.Generator
+) -> FireSimulation:
+    try:
+        logging.getLogger(__name__).info(f"Trying to create sim with seed {seed}")
+        sim_yaml_data["operational"].update({"seed": seed})
+        sim = FireSimulation(Config(config_dict=sim_yaml_data))
+        return sim
+    except FileNotFoundError as e:
+        logging.getLogger(__name__).error(e)
+        new_seed = rng.integers(low=1, high=1000, size=1, dtype=int)[0]
+        return _try_to_create_sim(sim_yaml_data, new_seed, rng)
