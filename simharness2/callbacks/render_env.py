95de9a0b357435fa62b8b9bc00c3387a2f2f2488
Aidan Fennelly <afennelly@mitre.org>
2023-10-16 18:03:08 -0700

    fix AttributeError in render_env.py

--- simharness2/callbacks/render_env.py
+++ simharness2/callbacks/render_env.py
@@ -90,7 +90,7 @@ class RenderEnv(DefaultCallbacks):
                 (within the vector of sub-environments of the BaseEnv).
             kwargs: Forward compatibility placeholder.
         """
-        env: ReactiveHarness = base_env.envs[env_index]
+        env: ReactiveHarness = base_env.get_sub_environments()[env_index]
 
         if worker.config.in_evaluation:
             logger.info("Creating evaluation episode...")
@@ -99,8 +99,8 @@ class RenderEnv(DefaultCallbacks):
                 logger.info("Enabling rendering for evaluation env.")
                 # TODO: Refactor below 3 lines into `env.render()` method?
                 os.environ["SDL_VIDEODRIVER"] = "dummy"
-                base_env.envs[env_index].sim.reset()
-                base_env.envs[env_index].sim.rendering = True
+                base_env.get_sub_environments()[env_index].sim.reset()
+                base_env.get_sub_environments()[env_index].sim.rendering = True
             elif not env._should_render and env.sim.rendering:
                 logger.error(
                     "Simulation is in rendering mode, but `env._should_render` is False."
@@ -138,7 +138,7 @@ class RenderEnv(DefaultCallbacks):
                 (within the vector of sub-environments of the BaseEnv).
             kwargs: Forward compatibility placeholder.
         """
-        env: ReactiveHarness = base_env.envs[env_index]
+        env: ReactiveHarness = base_env.get_sub_environments()[env_index]
         # Save a GIF from the last episode
         # TODO: Do we also want to save the fire spread graph?
         if worker.config.in_evaluation:
@@ -158,7 +158,7 @@ class RenderEnv(DefaultCallbacks):
                 )
                 # FIXME: Can we save each gif in a folder that relates it to episode iter?
                 logger.info(f"Saving GIF to {gif_save_path}...")
-                base_env.envs[env_index].sim.save_gif(gif_save_path)
+                base_env.get_sub_environments()[env_index].sim.save_gif(gif_save_path)
                 # Save the gif_path so that we can write image to aim server, if desired
                 # NOTE: `save_path` is a list after the above; do element access for now
                 logger.debug(f"Type of gif_save_path: {type(gif_save_path)}")
