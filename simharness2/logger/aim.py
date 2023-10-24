ef7cfa13be9f86ee5a9dc2622e59de5e11205493
Aidan Fennelly <afennelly@mitre.org>
2023-10-23 20:20:44 -0700

    update aim logger to current revision in dev

--- simharness2/logger/aim.py
+++ simharness2/logger/aim.py
@@ -1,12 +1,12 @@
-"""Module for using AIM with simharness2."""
+"""Module for using Aim with SimHarness2."""
 import logging
 from functools import partial
-from typing import TYPE_CHECKING, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 import numpy as np
 from aim import Image
 from hydra.utils import instantiate
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 from ray.tune.logger.logger import LoggerCallback
 from ray.tune.result import (
     EPISODES_TOTAL,
@@ -26,12 +26,6 @@ except ImportError:
     Repo, Run = None, None
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-handler.setFormatter(
-    logging.Formatter("%(asctime)s\t%(levelname)s %(filename)s:%(lineno)s -- %(message)s")
-)
-logger.addHandler(handler)
-logger.propagate = False
 
 VALID_SUMMARY_TYPES = [int, float, np.float32, np.float64, np.int32, np.int64]
 
@@ -120,8 +114,7 @@ class AimLoggerCallback(LoggerCallback):
         # Make temp file and write the trial params to it
         # tmp_path = os.path.join(trial.logdir, "expr_param_file.json")
         # with open(tmp_path, "w") as f:
-        #     json.dump(trial.config, f, indent=2, sort_keys=True,
-        # cls=SafeFallbackEncoder)
+        # json.dump(trial.config, f, indent=2, sort_keys=True, cls=SafeFallbackEncoder)
         # # Load temp file into dictionary, then delete the temp file
         # with open(tmp_path, "r") as f:
         #     params_dict = json.load(f)
@@ -185,12 +178,28 @@ class AimLoggerCallback(LoggerCallback):
         # gif_path = './trajectory.gif'
         # aim_image = aim.Image(image=gif_path, format='gif')
         # aim_run.track(value=aim_image, name=name, step=step_, context=context)
+        # NOTE: Gifs can only be saved to Aim UI if they are from evaluation episodes.
         if tmp_result.get("evaluation", None):
-            eval_episode_media = tmp_result["evaluation"].pop("episode_media")
-            if eval_episode_media:
-                for k, v in eval_episode_media.items():
-                    eval_gif = Image(v[0], caption="")
-                    trial_run.track(eval_gif, name=k, step=episode or step)
+            media: Dict[Any, List] = tmp_result["evaluation"].pop("episode_media", None)
+            # Ensure that there is episode media to log
+            if media and media.get("gif_data", None):
+                # Log gif for each episode added to episode media
+                for gif_data in media.get("gif_data"):
+                    # Prepare Aim Image object
+                    caption = gif_data.get("caption", "")
+                    image_path = gif_data.get("path", None)
+                    gif_img = Image(image_path, caption=caption)
+
+                    name = gif_data.get("name", None)
+                    # Use specified "step"; default to total episodes run
+                    episode = gif_data.get("step", episode)
+                    context = gif_data.get("context", {})
+                    trial_run.track(
+                        gif_img,
+                        name=name,
+                        step=episode or step,
+                        context=context,
+                    )
 
         flat_result = flatten_dict(tmp_result, delimiter="/")
         valid_result = {}
@@ -383,26 +392,3 @@ class AimLoggerCallback(LoggerCallback):
 
         cfg.evaluation_config = eval_cfg_settings
         run["evaluation"] = cfg
-
-    def _get_partial_targets(self, dict_cfg: DictConfig) -> set:
-        """Retrieve values in `cfg` that will be functools.partial objects."""
-        is_partial = set()
-        cfg = OmegaConf.to_container(dict_cfg)
-        for k, v in flatten_dict(cfg).items():
-            if "_partial_" in k:
-                is_partial.add(k.split("/")[-2])
-
-        # WIP: splitting out target class names. I want to log:
-        # - partial_target_name: target_class (ie "reward_cls_partial:
-        # simharness2.rewards.ComprehensiveReward")
-        # - partial_target_name: kw args as tuple (ie. "reward_cls_partial: (penalty)")
-        # is_partial_target = set()
-        # partial_target_cls = {}
-        # flat_cfg = flatten_dict(tmp_cfg)
-        # for k,v in flat_cfg.items():
-        #     if "_partial_" in k:
-        #         partial_cfg_name = "/".join([x for x in k.split("/")[:-1]])
-        #         is_partial_target.add(partial_cfg_name)
-        #         # get corresponding target class
-        #         trg_cls = flat_cfg[f"{partial_cfg_name}/_target_"]
-        #         partial_target_cls[partial_cfg_name] = trg_cls
