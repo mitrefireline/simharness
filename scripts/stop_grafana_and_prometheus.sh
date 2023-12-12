030d6cddfd4b615327e7311942b9492c4a5c9617
Aidan Fennelly <afennelly@mitre.org>
2023-12-12 13:46:16 -0800

    Delete docker/, k8s/, scripts/, and setup/ dirs

--- scripts/stop_grafana_and_prometheus.sh
+++
@@ -1,9 +0,0 @@
-# Assumption: strictly 1 raycluster is currently deployed on AIP
-# HEAD_POD=$(kubectl get pods | grep raycluster | grep head | awk '{ print $1 }')
-HEAD_POD=$(kubectl get pods --selector=ray.io/node-type=head -o custom-columns=POD:metadata.name --no-headers)
-
-# Interrupt prometheus from within the prometheus tmux session
-kubectl exec $HEAD_POD -- bash -c "tmux send-keys -t prometheus.0 C-c"
-
-# Interrupt grafana from within the grafana tmux session
-kubectl exec $HEAD_POD -- bash -c "tmux send-keys -t grafana.0 C-c"
