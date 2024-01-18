2cbccd1f2926cf01528b3c90b8599980bfad64bc
William Macke <wmacke@mitre.org>
2024-01-18 16:47:57 -0500

    [WIP] added first draft of custom multimodal model, still needs to properly format action

--- simharness2/models/__init__.py
+++ simharness2/models/__init__.py
@@ -1,8 +1,13 @@
 from ray.rllib.models.catalog import ModelCatalog
 
 from .custom_dqn_torch_model import CustomDQNTorchVisionNet
+from .custom_multimodal_torch_model import CustomMultimodalTorchModel
 
 # Register custom model.
 ModelCatalog.register_custom_model(
     "metric_reporting_vision_network", CustomDQNTorchVisionNet
+)
+
+ModelCatalog.register_custom_model(
+    "multimodal_network", CustomMultimodalTorchModel
 )
