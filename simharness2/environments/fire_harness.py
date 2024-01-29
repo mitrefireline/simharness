48498ccb17072ce61c29c0471400b14b96e67b8d
William Macke <wmacke@mitre.org>
2024-01-29 12:07:03 -0500

    added various bug fixes and implementations of multimodal environment

--- simharness2/environments/fire_harness.py
+++ simharness2/environments/fire_harness.py
@@ -175,8 +175,8 @@ class FireHarness(Harness[AnyFireSimulation]):
         reward = self.reward_cls.get_reward(
             timestep=self.timesteps,
             sim_run=sim_run,
-            agents=self.agents,
-            agent_speed=self.agent_speed,
+            #agents=self.agents,
+            #agent_speed=self.agent_speed,
         )
 
         # FIXME account for below updates in the reward_cls.calculate_reward() method
--- simharness2/models/custom_multimodal_torch_model.py
+++ simharness2/models/custom_multimodal_torch_model.py
@@ -1,9 +1,10 @@
 """Custom model for running on multimodel data"""
+
 from typing import Dict, List
 
 import torch
 from gymnasium.spaces import Space
-from ray.rllib.models.torch.misc import SlimConv2d, same_padding
+from ray.rllib.models.torch.misc import SlimConv2d, same_padding, SlimFC
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils.annotations import override
@@ -33,7 +34,7 @@ class CustomMultimodalTorchModel(TorchModelV2, torch.nn.Module):
 
         filters = self.model_config["conv_filters"]
         activation = self.model_config.get("conv_activation")
-        (w, h, in_channels) = obs_space[FIRE_MAP_KEY].shape
+        (w, h, in_channels) = obs_space.original_space[FIRE_MAP_KEY].shape
         in_size = [w, h]
         layers = []
         for out_channels, kernel, stride in filters:
@@ -51,19 +52,30 @@ class CustomMultimodalTorchModel(TorchModelV2, torch.nn.Module):
             in_channels = out_channels
             in_size = out_size
         in_size = (
-            out_size[0] * out_size[1] * out_channels + obs_space[POSITION_KEY].shape[0]
+            out_size[0] * out_size[1] * out_channels
+            + obs_space.original_space[POSITION_KEY].shape[0]
         )
         layers.append(torch.nn.Flatten())
         fc_layers = []
         fc_layers_value = []
         for out_size in self.model_config["fcnet_hiddens"]:
-            fc_layers.append(torch.nn.Linear(in_size, out_size))
-            fc_layers.append(torch.nn.ReLU())
-            fc_layers_value.append(torch.nn.Linear(in_size, out_size))
-            fc_layers_value.append(torch.nn.ReLU())
+            fc_layers.append(
+                SlimFC(
+                    in_size,
+                    out_size,
+                    activation_fn=torch.nn.ReLU,
+                )
+            )
+            fc_layers_value.append(
+                SlimFC(
+                    in_size,
+                    out_size,
+                    activation_fn=torch.nn.ReLU,
+                )
+            )
             in_size = out_size
-        fc_layers.append(torch.nn.Linear(in_size, action_space.n))
-        fc_layers_value.append(torch.nn.Linear(in_size, 1))
+        fc_layers.append(SlimFC(in_size, num_outputs))
+        fc_layers_value.append(SlimFC(in_size, 1))
         self._conv_model = torch.nn.Sequential(*layers)
         self._fc_model = torch.nn.Sequential(*fc_layers)
         self._fc_value = torch.nn.Sequential(*fc_layers_value)
@@ -75,15 +87,17 @@ class CustomMultimodalTorchModel(TorchModelV2, torch.nn.Module):
         state: List[TensorType],
         seq_lens: TensorType,
     ) -> (TensorType, List[TensorType]):
-        fire_map = input_dict[SampleBatch.OBS][FIRE_MAP_KEY]
-        position = input_dict[SampleBatch.OBS][POSITION_KEY]
+        if SampleBatch.OBS in input_dict and "obs_flat" in input_dict:
+            orig_obs = input_dict[SampleBatch.OBS]
+        else:
+            orig_obs = restore_original_dimensions(
+                input_dict[SampleBatch.OBS], self.obs_space, "torch"
+            )
+        fire_map = orig_obs[FIRE_MAP_KEY]
+        fire_map = torch.transpose(fire_map, 1, -1)
+        position = orig_obs[POSITION_KEY]
         conv_out = self._conv_model(fire_map)
         fc_input = torch.cat([conv_out, position], dim=-1)
         self._features = fc_input
         out = self._fc_model(fc_input)
         return out, state
-
-    @override(TorchModelV2)
-    def value_function(self) -> TensorType:
-        return self._fc_value(self._features).squeeze(1)
-
