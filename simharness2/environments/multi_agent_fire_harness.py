e65e54082206130cf3bd28b30dd54b7dab5d8048
Aidan Fennelly <afennelly@mitre.org>
2024-02-29 13:45:58 -0800

    Updates to make MultiAgentFireHarness work as desired

--- simharness2/environments/multi_agent_fire_harness.py
+++ simharness2/environments/multi_agent_fire_harness.py
@@ -101,26 +101,17 @@ class MultiAgentFireHarness(FireHarness[AnyFireSimulation], MultiAgentEnv):
         # TODO pass `terminated` into `get_reward` method
         # FIXME: Update reward for MARL case!!
         # TODO: Give each agent the "same" simple reward for now.
-        reward = self.reward_cls.get_reward(self.timesteps, sim_run)
+        reward = self.reward_cls.get_reward(
+            timestep=self.timesteps,
+            sim_run=sim_run,
+            # agents=self.agents,
+            # agent_speed=self.agent_speed,
+        )
 
-        # Terminate episode early if burn damage in Agent Sim is larger than final bench fire map
-        self._terminate_if_greater_damage = False #FIXME get rid of this line when the fixme in the if statement below is implemented
-        if self.benchmark_sim:
-            if self._terminate_if_greater_damage:
-                total_area = self.sim.fire_map.size
-
-                sim_damaged_total = self.harness_analytics.sim_analytics.data.burned + self.harness_analytics.sim_analytics.data.burning
-                # FIXME Fix this damage calculation if needed to account for damage across all the agent sims
-                benchsim_damaged_total = total_area - self.harness_analytics.benchmark_sim_analytics.data.unburned
-
-                if sim_damaged_total > benchsim_damaged_total:
-                    terminated = True
-                    # TODO potentially add a static negative penalty for making the fire worse
-
-        # TODO account for below updates in the reward_cls.calculate_reward() method
+        # FIXME account for below updates in the reward_cls.calculate_reward() method
         # "End of episode" reward
-        if terminated:
-            reward += 10
+        # if terminated:
+        # reward += 10
 
         if self.harness_analytics:
             self.harness_analytics.update_after_one_harness_step(
@@ -128,9 +119,11 @@ class MultiAgentFireHarness(FireHarness[AnyFireSimulation], MultiAgentEnv):
             )
 
         rewards, truncateds, terminateds, infos = {}, {}, {}, {}
+        # new_obs, rewards, truncateds, terminateds, infos = {}, {}, {}, {}, {}
         truncs = set()
         terms = set()
         for agent_id, agent in self.agents.items():
+            # new_obs[agent_id] = self.state
             # FIXME: All agents receive the SAME reward !!!
             rewards[agent_id] = reward
             # FIXME: Trunc/Term logic is the SAME for all agents.
@@ -149,6 +142,7 @@ class MultiAgentFireHarness(FireHarness[AnyFireSimulation], MultiAgentEnv):
 
         self.timesteps += 1  # increment AFTER method logic is performed (convention).
 
+        # return new_obs, rewards, terminateds, truncateds, infos
         return self.state, rewards, terminateds, truncateds, infos
 
     def _parse_action(self, action: np.ndarray) -> Tuple[int, int]:
