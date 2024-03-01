c851cd1786e270c339780f7019481fd5938e3c8e
Aidan Fennelly <afennelly@mitre.org>
2024-03-01 08:14:22 -0800

    WIP: updates to .py files for testing pipeline before merge

--- simharness2/environments/complex_harness.py
+++ simharness2/environments/complex_harness.py
@@ -6,7 +6,10 @@ from gymnasium import spaces
 from simfire.sim.simulation import FireSimulation
 
 from simharness2.environments.fire_harness import ReactiveHarness
-from simharness2.models.custom_multimodal_torch_model import FIRE_MAP_KEY, POSITION_KEY
+from simharness2.models.custom_multimodal_torch_model import (
+    FIRE_MAP_KEY,
+    AGENT_POSITION_KEY,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -21,10 +24,10 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
         super().__init__(**kwargs)
 
         # FIXME: Expand to include SimFire data layers (ie. `self.sim_attributes`).
-        # if self.attributes != [FIRE_MAP_KEY, POSITION_KEY]:
+        # if self.attributes != [FIRE_MAP_KEY, AGENT_POSITION_KEY]:
         #     raise AssertionError(
         #         f"The `ComplexObsReactiveHarness` requires `self.attributes` to be "
-        #         f"[{FIRE_MAP_KEY}, {POSITION_KEY}]."
+        #         f"[{FIRE_MAP_KEY}, {AGENT_POSITION_KEY}]."
         #     )
 
         if self.num_agents > 1:
@@ -44,7 +47,7 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
         pos_state = default_agent.get_normalized_position(max_x=max_x, max_y=max_y)
         nonsim_data = {
             FIRE_MAP_KEY: np.copy(self.sim.fire_map),
-            POSITION_KEY: pos_state,
+            AGENT_POSITION_KEY: pos_state,
         }
         return nonsim_data
 
@@ -57,7 +60,7 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
         pos_bounds = {"min": 0, "max": max(self.sim.fire_map.shape) - 1}
         nonsim_min_maxes = {
             FIRE_MAP_KEY: fire_map_bounds,
-            POSITION_KEY: pos_bounds,
+            AGENT_POSITION_KEY: pos_bounds,
         }
         return nonsim_min_maxes
 
@@ -81,13 +84,13 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
 
         return {
             FIRE_MAP_KEY: np.stack(fire_map_obs, axis=-1).astype(np.float32),
-            POSITION_KEY: pos_state,
+            AGENT_POSITION_KEY: pos_state,
         }
 
     # FIXME: Add new logic. Current code is just a placeholder.
     def get_initial_state(self) -> np.ndarray:
         """TODO."""
-        #fire_map_state = super().get_initial_state()
+        # fire_map_state = super().get_initial_state()
         sim_observations = super()._select_from_dict(
             self.sim.get_attribute_data(), self.sim_attributes
         )
@@ -107,7 +110,7 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
 
         return {
             FIRE_MAP_KEY: np.stack(fire_map_obs, axis=-1).astype(np.float32),
-            POSITION_KEY: pos_state,
+            AGENT_POSITION_KEY: pos_state,
         }
 
     def get_observation_space(self) -> spaces.Space:
@@ -115,23 +118,23 @@ class ComplexObsReactiveHarness(ReactiveHarness[AnyFireSimulation]):
         return spaces.Dict(
             {
                 FIRE_MAP_KEY: self._get_fire_map_observation_space(),
-                POSITION_KEY: self._get_position_observation_space(),
+                AGENT_POSITION_KEY: self._get_position_observation_space(),
             }
         )
 
     def _get_fire_map_observation_space(self) -> spaces.Box:
         """TODO."""
-        # Ensure POSITION_KEY is not in `self.sim_attributes`.
+        # Ensure AGENT_POSITION_KEY is not in `self.sim_attributes`.
         """
-        if POSITION_KEY in self.nonsim_attributes:
-            self.nonsim_attributes.pop(self.nonsim_attributes.index(POSITION_KEY))
+        if AGENT_POSITION_KEY in self.nonsim_attributes:
+            self.nonsim_attributes.pop(self.nonsim_attributes.index(AGENT_POSITION_KEY))
         """
 
         fire_map_attributes = self.attributes.copy()
-        fire_map_attributes.pop(fire_map_attributes.index(POSITION_KEY))
+        fire_map_attributes.pop(fire_map_attributes.index(AGENT_POSITION_KEY))
         # FIXME: Does custom model expect channel-minor or channel-major format?
         # NOTE: calling `reshape()` to switch to channel-minor format.
-        # TODO: Note that the following attributes DO NOT reflect POSITION_KEY.
+        # TODO: Note that the following attributes DO NOT reflect AGENT_POSITION_KEY.
         self._channel_lows = np.array(
             [[[self.min_maxes[channel]["min"]]] for channel in fire_map_attributes]
         ).reshape(1, 1, len(fire_map_attributes))
