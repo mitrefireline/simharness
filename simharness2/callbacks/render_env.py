b9fb45c97db0ba64e228b5f2d02f3f57f0042fb8
Aidan Fennelly <afennelly@mitre.org>
2024-03-14 11:18:12 -0700

    Update reset behavior of analytics in on_episode_created()

--- simharness2/callbacks/render_env.py
+++ simharness2/callbacks/render_env.py
@@ -123,8 +123,11 @@ class RenderEnv(DefaultCallbacks):
             )
             env._configure_env_rendering(True)
 
-            # FIXME: Remove later, using for debugging purposes!!
-            # env.harness_analytics.reset(env_is_rendering=True)
+            # TODO: Look into save_history behavior in analytics module.
+            env.harness_analytics.reset(
+                env_is_rendering=True,
+                reset_benchmark=env._new_fire_scenario,
+            )
 
     def should_render_env(
         self, env: "FireHarness[FireSimulation]", env_type: str
